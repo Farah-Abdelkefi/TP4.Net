9487fed8639eb3164b1889e5707c3d222c889d56
FarahAbdelkefi <farah.abdelkefi@gmail.com>
2022-12-16 16:38:34 +0100

    first commit

---
+++ TP4/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TP4.Data;
+using TP4.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace TP4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+         
+               
+                UniversityContext universityContext = UniversityContext.Instantiate_UniversityContext();
+                StudentRepository studentRep = new StudentRepository(universityContext);
+                IEnumerable<String> s = studentRep.GetCourses();
+                return View(s);
+             
+        }
+
+        public IActionResult getcourse(string id)
+        {
+            UniversityContext universityContext = UniversityContext.Instantiate_UniversityContext();
+            StudentRepository studentRep = new StudentRepository(universityContext);
+            IEnumerable<Student> students = (IEnumerable<Student>) studentRep.Find((s) => s.course.ToLower() == id.ToLower());
+            
+            ViewBag.course = id;
+            if (students.Count()==0) { ViewBag.NotFound = true; } 
+            return View(students);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TP4/Data/StudentRepository.cs
@@ -0,0 +1,27 @@
+﻿using TP4.Models;
+using Microsoft.EntityFrameworkCore;
+namespace TP4.Data
+{
+    public interface IStudentRepository:IRepository<Student>
+    {
+           IEnumerable<String> GetCourses();
+    }
+    public class StudentRepository : Repository<Student>, IStudentRepository
+    {
+        private readonly UniversityContext _context;
+        public StudentRepository(UniversityContext db_context) : base(db_context)
+        {
+            this._context = db_context;
+        }
+
+        
+        public IEnumerable<String> GetCourses()
+        {
+            return _context.Student.Select(s =>s.course).Distinct().ToList();
+
+        }
+
+        
+
+    }
+}
